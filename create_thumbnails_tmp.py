68d2d22066ea5d36285d6662c6cf9ad4f7311d19
polmic <polmic31@gmail.com>
2020-02-20 14:41:44 +0100

    Update create_thumbnails_tmp.py

--- create_thumbnails_tmp.py
+++ create_thumbnails_tmp.py
@@ -1,12 +1,13 @@
 from google_images_download import google_images_download   #importing the library
 import requests
 import os
+import base64
 from os import listdir
 from os.path import isfile, join
 
 api_description_endpoint = "http://localhost:4000/plant-description/"
 api_thumbnail_endpoint = "http://localhost:4000/plant-thumbnail/"
-download_folder_path = "D:/workspace/test/google-images-download/downloads"
+download_folder_path = "/home/pmicas/workspace/repositories/google-images-download/downloads"
 
 gim = google_images_download.googleimagesdownload()   #class instantiation
 
@@ -14,7 +15,7 @@ r = requests.get(api_description_endpoint)
 descriptions = (r.json())
 
 for description in descriptions:
-    commonName = description['commonName']
+    commonName = description['commonName'].split(',')[0]
     arguments = {
         "keywords": commonName,
         "limit": 10,
@@ -23,28 +24,30 @@ for description in descriptions:
     }
     print(arguments)
     paths = gim.download(arguments)
-    print(paths)
 
     images_folder_path = download_folder_path + "/" + commonName
 
     i = 0
       
     for filename in listdir(images_folder_path):
-        dst =str(i) + ".png"
-        src =images_folder_path + filename 
-        dst =images_folder_path + dst 
-          
+        extension = filename[-4:]
+        dst = str(i) + extension
+        src = images_folder_path + "/" + filename
+        dst = images_folder_path + "/" + dst 
         os.rename(src, dst) 
         i += 1
 
     images = [f for f in listdir(images_folder_path) if isfile(join(images_folder_path, f))]
 
+    print('__________________\n')
+    print(images)
+    print('__________________')
+
     for image in images:
-        print('-- - - - ')
-        print(image)
-        data = open(image, 'rb').read()
+        binary = base64.b64encode(open(images_folder_path + "/" +image, 'rb').read()).decode('ascii')
         res = requests.post(
-            url=api_thumbnail_endpoint,
-            data=data,
+            url=api_thumbnail_endpoint + "/" + description['_id'],
+            data=binary,
             headers={'Content-Type': 'application/octet-stream'}
         )
+        print(image + ' : ' + str(res.status_code))
